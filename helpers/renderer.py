5f88443433c4e9388b125e7b01b98224c365ad3e
Bru Ronchi <brunom.ronchi@gmail.com>
2022-09-04 16:50:08 -0300

    Renderer class sketch

--- helpers/renderer.py
+++ helpers/renderer.py
@@ -1,4 +1,5 @@
-from typing import Union
+from collections import defaultdict
+from typing import Optional, Union
 import io
 import os
 import sys
@@ -6,6 +7,47 @@ import sys
 from pathlib import Path
 import cairosvg
 import yaml
+
+class Renderer:
+
+    @staticmethod
+    def remap(ch):
+        REMAP = {"<": "&lt;", ">": "&gt;", '"': "&quot;"}
+        if ch in REMAP:
+            return REMAP[ch]
+        return ch
+
+    def formatted_character(self, i: int, j: int, ch: str):
+        fill = self.colors[(i, j)]
+        style = f"font-family:'{self.fonts[(i, j)]}'"
+        size = self._font_size
+        y = 1 + (i + 1) * self._vstep
+        x = 1 + j * self._hstep
+        x += self.offsets[ch] * self._hstep
+        ch = self.remap(ch)
+        return (
+            f'    <text x={x}px" y="{y}px" font-size="{self._font_size}pt" '
+                 f'fill="#{fill}" style="{style}">{ch}</text>')
+
+    def __init__(self, width=1366, height=768, h_chars=79, v_chars=22,
+        offsets: Optional[defaultdict]=None, default_color="#00FFAF",
+        default_font="CaskaydiaCove Nerd Font Mono",):
+        self.default_color = default_color
+        self.default_font = default_font
+        self.colors = defaultdict(lambda: self.default_color)
+        self.fonts = defaultdict(lambda: self.default_font)
+        if offsets is None:
+            offsets = defaultdict(lambda: 0)
+            offsets["í"] = offsets["Í"] = 2 / 7
+        else:
+            self.offsets = offsets
+        self._width = width
+        self._height = height
+        self._hstep = (width - 2) / h_chars
+        self._vstep = (height - 2) / v_chars
+        # 1pt = 96/72 px
+        # 6/7th of a step for the font
+        self._font_size = self._vstep * 96 * 6 / 7 / 72 
 
 def parse(content: str) -> dict:
     split = content.split("\n", 22)
